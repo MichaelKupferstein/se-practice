5c493fdd350ef9bbf72beb186920b74c27f70381
Michael Kupferstein <mkupfers@mail.yu.edu>
2023-05-09 17:43:03 -0400

    some of this

--- project/src/main/java/edu/yu/cs/com/project/people/Guest.java
+++ project/src/main/java/edu/yu/cs/com/project/people/Guest.java
@@ -4,6 +4,8 @@ import edu.yu.cs.com.project.people.Person;
 
 public class Guest extends Person {
 
+    private int cardNumber;
+
     public Guest(String name, int id){
         this.name = name;
         this.ID = id;
@@ -13,5 +15,11 @@ public class Guest extends Person {
         this.age = age;
     }
 
+    public void setCreditCardNumber(int cardNumber){
+        this.cardNumber = cardNumber;
+    }
 
+    public int getCardNumber() {
+        return this.cardNumber;
+    }
 }
